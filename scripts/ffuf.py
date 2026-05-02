0abc45e30ec63a08a2a8bee8b0feffbec79e0ace
Pillon97 <kriskomaxi97@gmail.com>
2026-05-02 14:08:10 +0200

    update

--- scripts/ffuf.py
+++ scripts/ffuf.py
@@ -7,7 +7,7 @@ import make_machine
 parser = argparse.ArgumentParser(description="FFUF URL Scanner")
 parser.add_argument("-i", "--input", help="Input file containing payloads (one per line)")
 parser.add_argument("-o", "--output", default="ffuf_scan.txt", help="Output file name (default: ffuf_scan.txt)")
-parser.add_argument("-u", "--url", default="http://example.com/FUZZ", help="Target URL with FUZZ keyword (default: http://example.com/FUZZ)")
+parser.add_argument("-u", "--url", default= input("Enter target URL:"), help="Target URL with FUZZ keyword (default: http://example.com/FUZZ)")
 parser.add_argument("-w", "--wordlist", default="~/Pillon_tool/tools/SecLists/Discovery/Web-Content/common.txt", help="Custom wordlist file for ffuf")
 args = parser.parse_args()
 url = "http://"+args.url+"/FUZZ"
