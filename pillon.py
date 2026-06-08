e18b886dbff0559107674a0bc6f8631c8db07ccb
Pillon97 <kriskomaxi97@gmail.com>
2026-06-08 13:10:59 +0200

    Fix installation script to use correct path for .bashrc during setup

--- pillon.py
+++ pillon.py
@@ -176,9 +176,9 @@ if __name__ == "__main__":
             run_install(dep["install"])
         folders()
         installed()
-        subprocess.run("export USER=$(pwd | awk -F '/' '{print $3}')", shell=True, check=True)
-        subprocess.run("sudo chown -R $USER:$USER /home/$USER/*", shell=True, check=True)
-        subprocess.run("cp default/.bashrc $HOME/.bashrc", shell=True, check=True)
+        subprocess.run('USER=$(pwd | awk -F/ "{print \\$3}"); '
+    'chown -R "$USER:$USER" "/home/$USER"; '
+    'cp default/.bashrc "/home/$USER/.bashrc"', shell=True, check=True)
         log_install("First-time setup completed successfully.")
         start()
         menu()
